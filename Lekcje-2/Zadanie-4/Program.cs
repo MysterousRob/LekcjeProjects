fa6eb5444265bfeb2cdb81ea147ddbdd97aeb17f
MysteriousRob <robertjohngasior@gmail.com>
2024-10-25 10:22:12 +0200

    updated lekcje 2 / zad-4

--- Lekcje-2/Zadanie-4/Program.cs
+++ Lekcje-2/Zadanie-4/Program.cs
@@ -30,21 +30,35 @@ Console.WriteLine("toDivide: " + toDivide);
 //Math.E
 //Math.Tau
 
+//nie jest potrxebna źeby to napisać 
+double sqrt = Math.Sqrt(9);  // Wynik: 3
+double pow = Math.Pow(3, 2);  // Wynik: 9
+int abs = Math.Abs(-10);  // Wynik: 10
+double sin = Math.Sin(1.57);  // Wynik: około 1 (sin(π/2) ≈ 1)
+double round1 = Math.Round(1.5);  // Wynik: 2
+double round2 = Math.Round(2.5);  // Wynik: 2
+double truncate = Math.Truncate(1.95);  // Wynik: 1
+double ceiling = Math.Ceiling(1.95);  // Wynik: 2
+double floor = Math.Floor(1.95);  // Wynik: 1
+int clampedValue = Math.Clamp(4, 0, 10);  // Wynik: 4 (w zakresie 0-10)
+int max = Math.Max(2, 6);  // Wynik: 6
+int min = Math.Min(2, 6);  // Wynik: 2
+int sign = Math.Sign(-2);  // Wynik: -1
 
-//double sqrt = Math.Sqrt(9);  // Wynik: 3
-//double pow = Math.Pow(3, 2);  // Wynik: 9
-//int abs = Math.Abs(-10);  // Wynik: 10
-//double sin = Math.Sin(1.57);  // Wynik: około 1 (sin(π/2) ≈ 1)
-//double round1 = Math.Round(1.5);  // Wynik: 2
-//double round2 = Math.Round(2.5);  // Wynik: 2
-//double truncate = Math.Truncate(1.95);  // Wynik: 1
-//double ceiling = Math.Ceiling(1.95);  // Wynik: 2
-//double floor = Math.Floor(1.95);  // Wynik: 1
-//int clampedValue = Math.Clamp(4, 0, 10);  // Wynik: 4 (w zakresie 0-10)
-//int max = Math.Max(2, 6);  // Wynik: 6
-//int min = Math.Min(2, 6);  // Wynik: 2
-//int sign = Math.Sign(-2);  // Wynik: -1
-
+//(tylko na testowanie)
+Console.WriteLine(sqrt);
+Console.WriteLine(pow);
+Console.WriteLine(abs);
+Console.WriteLine(sin);
+Console.WriteLine(round1);
+Console.WriteLine(round2);
+Console.WriteLine(truncate);
+Console.WriteLine(ceiling);
+Console.WriteLine(floor);
+Console.WriteLine(clampedValue);
+Console.WriteLine(max);
+Console.WriteLine(min);
+Console.WriteLine(sign);
 
 //double pi = Math.PI;  
 //double e = Math.E; 
