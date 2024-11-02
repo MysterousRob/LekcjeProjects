436372bdf139b893e6c411c8328587cd52a8a609
MysteriousRob <robertjohngasior@gmail.com>
2024-11-02 10:34:26 +0100

    zrobione do zadanie 2 lek 4 skonczone i robi bylko pare zadan w lek 3 nie robia

--- Lekcje-3/Zadanie-5/obj/Debug/net8.0/Zadanie-5.AssemblyInfo.cs
+++ Lekcje-3/Zadanie-5/obj/Debug/net8.0/Zadanie-5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zadanie-5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b6d54c04c3cb4dffef4785ad6ddc2473af34bfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f11567928d1aa78532698303c190fe3b36d373e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zadanie-5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zadanie-5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lekcje-4/Zadanie-2/Program.cs
@@ -0,0 +1,12 @@
+﻿void Swap(ref double a, ref double b)
+{
+    double temp = a;
+    a = b;
+    b = temp;
+}
+
+double x = 10;
+double y = 20;
+
+Swap(ref x, ref y);
+Console.WriteLine($"x = {x}, y = {y}");
